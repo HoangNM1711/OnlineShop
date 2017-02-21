bbe270587131d6aed1d78150cc86fe17b216de42
hoangnm1711@gmail.com <kingfight1412>
2017-02-21 08:58:04 +0700

    Override BaseController

--- OnlineShop/Areas/Admin/Controllers/LoginController.cs
+++ OnlineShop/Areas/Admin/Controllers/LoginController.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Model.DAO;
-
+using OnlineShop.Common;
 
 namespace OnlineShop.Areas.Admin.Controllers
 {
@@ -19,23 +19,35 @@ namespace OnlineShop.Areas.Admin.Controllers
 
         public ActionResult Login(LoginModel model)
         {
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
                 var dao = new UserDAO();
-                var result = dao.Login(model.Username, model.Password);
-                if (result)
+                var result = dao.Login(model.Username, Encryptor.MD5Hash(model.Password));
+                if (result == 1)
                 {
                     var user = new UserDAO().GetByID(model.Username);
                     var session = new UserLogin();
                     session.Username = user.UserName;
                     session.UserID = user.ID;
-                    Session.Add(Common.Constants.USER_SESSION,session);
+                    Session.Add(Common.Constants.USER_SESSION, session);
 
                     return RedirectToAction("Index", "Home");
                 }
+                else if (result == 0)
+                {
+                    ModelState.AddModelError("", "Tài khoản không tồn tại");
+                }
+                else if (result == -1)
+                {
+                    ModelState.AddModelError("", "Tài khoản bị khóa");
+                }
+                else if (result == -2)
+                {
+                    ModelState.AddModelError("", "Sai mật khẩu");
+                }
                 else
                 {
-                    ModelState.AddModelError("", "Đăng nhập không thành công");
+                    ModelState.AddModelError("", "Tài khoản không tồn tại");
                 }
             }
             return View("Index");
